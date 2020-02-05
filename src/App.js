3f0a95df4755e4fde314cd06e742171baf0294ae
Heber <hebergonza@gmail.com>
2020-02-04 21:09:53 -0500

    moved reducer directory to src directory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { createStore } from "redux";
+
+import {carReducer} from "../src/"
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+
+const store = createStore();
 
 const App = () => {
   const state = {
